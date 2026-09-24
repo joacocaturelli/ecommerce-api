6016f5e3e306668c65bc1faf7f9342509c1adb11
joacocaturelli <joacocatu@gmail.com>
2026-09-24 13:17:09 +0200

    Fix: eliminada linea, no es valida en express

--- src/app.js
+++ src/app.js
@@ -35,7 +35,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.post("/api/stripe/webhook", express.raw({ type: "application/json" }), stripeWebhook);
 
